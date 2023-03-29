973f8c9c67acede1a223d648345e47f605b647e4
nilesh9766814824 <nileshchoudhary585@gmail.com>
2023-03-29 22:06:22 +0530

    .

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Bussiness from "./Bussiness";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import Getstarted from "./Getstarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+
+export {
+  Billing,
+  Bussiness,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  Feedback,
+  Footer,
+  Getstarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonial
+}
