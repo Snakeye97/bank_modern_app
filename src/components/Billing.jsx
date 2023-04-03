4b4a3c16246641daaa20c3719d8ec761df0abef0
Nilesh <117421926+Snakeye97@users.noreply.github.com>
2023-04-03 16:09:16 +0530

    .

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { apple,bill,google } from '../assets'
 import styles, {layout} from '../style'
 
-const Billing = () => (
+const  Billing = () => (
     <section id='product' className={layout.sectionReverse}>
       <div className={layout.sectionImgReverse}>
         <img src={bill} alt="bill" className='w-[100%] h-[100%] relative z-[5] ' />
