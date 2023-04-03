fac4c889d232cc849ebe99bd3d45d20abd781dd2
Nilesh <117421926+Snakeye97@users.noreply.github.com>
2023-04-03 22:02:13 +0530

    .

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type='button' className={`font-poppins font-medium text-[18px] text-primary outline-none ${styles} py-4 px-6 bg-blue-gradient`}>
+    <button type='button' className={`font-poppins font-medium text-[18px] text-primary outline-none ${styles} py-4 px-6 bg-blue-gradient rounded-[10px]`}>
       Get started
     </button>
   )
